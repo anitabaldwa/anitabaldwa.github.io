c9186397d7e31d59c114909421902f2b6ebafd0d
Devansh Baldwa <devanshbaldwa@gmail.com>
2020-06-17 19:16:02 +0530

    Navbar few changes done

--- src/components/my-nav-bar.jsx
+++ src/components/my-nav-bar.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
-import '../App.css';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 
 function MyNavBar(){
     return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
-  <Navbar.Brand class="mynavbar-padding" href="#home">Anita Baldwa</Navbar.Brand>
+  <Navbar.Brand className="my-padding-navbar-left" style={{fontSize:'30px'}} href="#home">Anita Baldwa</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
-    
-    <Nav className="ml-auto">
- 
-      <Nav.Link class="mynavbar-padding" href="#features">Insurance?</Nav.Link>
-      <Nav.Link class="mynavbar-padding" href="#pricing">About Me</Nav.Link>
-      <NavDropdown class="mynavbar-padding" title="Dropdown" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+    <div className="my-padding-navbar-right">
+    <Nav className="mr-auto">
+      <Nav.Link className="my-navbar-size16" href="#features">About Insurance</Nav.Link>
+      <Nav.Link className="my-navbar-size16" href="#pricing">About Me</Nav.Link>
+      <NavDropdown className="my-navbar-size16" title="Companies" id="collasible-nav-dropdown">
+        <div className="my-navbar-size14">
+        <NavDropdown.Item href="#action/3.1">Aditya Birla Capital</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">MaxBupa Health Insurance</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Bajaj Allianz General Insurance</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Mutual Funds</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.4">NJ Wealth</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.4">Vehicle Insurance of All Companies</NavDropdown.Item></div>
       </NavDropdown>
-      <Nav.Link class="mynavbar-padding" href="#deets">Contact</Nav.Link>      
-    </Nav>
+      {/* <Nav.Link class="mynavbar-padding" href="#LifeInsurance">Life Insurance</Nav.Link>
+      <Nav.Link class="mynavbar-padding" href="#HealthInsurance">Health Insurance</Nav.Link>
+      <Nav.Link class="mynavbar-padding" href="#MutualFunds">Mutual Funds</Nav.Link> */}
+      <Nav.Link className="mynavbar-padding my-navbar-size16" href="#deets">Contact</Nav.Link>      
+    </Nav></div>
   </Navbar.Collapse>
 </Navbar>
     );
